5647c07dffb760db3620357396102a316d1616b6
Walton Lee <mr.waltonlee@me.com>
2017-01-04 05:17:48 +0000

    revising to a single print statement

--- 0x01-python-if_else_loops_functions/100-print_tebalpha.py
+++ 0x01-python-if_else_loops_functions/100-print_tebalpha.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 for i in range (122, 96, -1):
-    if(i % 2 == 0):
-        print('{:.26}'.format(chr(i)), end="")
-    else:
-        i = chr(i - 32)
-        print('{:.26}'.format(i), end="")
+    if(i % 2 != 0):
+        i = (i - 32)
+    print('{}'.format(chr(i)), end="")
