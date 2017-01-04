69cb1e088f697a30fa5565eecb8d83ebea2cf1b6
Walton Lee <mr.waltonlee@me.com>
2017-01-04 00:46:00 +0000

    printing out all different combinations of two digits

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for i in range (0, 10):
+    for a in range (0, 10):
+        if(i < a and not (i == 8 and a == 9)):
+            print('{:d}{:d}, '.format(i, a), end="")
+print('{:d}'.format(89))
