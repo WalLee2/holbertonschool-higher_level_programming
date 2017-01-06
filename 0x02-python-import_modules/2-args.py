1f37e0f163db1a7d6c14e777241dcbdac9432e7e
Walton Lee <mr.waltonlee@me.com>
2017-01-06 01:51:46 +0000

    removing a colon

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     if(length - 1 == 0):
         print('{:d} argument.'.format(length - 1))
     elif(length - 1 <= 1):
-        print('{:d}: argument: \n{:d}: {:s}'
+        print('{:d} argument: \n{:d}: {:s}'
               .format((length - 1), (length - 1), sys.argv[1]))
     elif(length > 2):
         print('{:d} arguments:'.format(length - 1))
