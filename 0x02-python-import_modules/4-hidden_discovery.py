db4306be690d7f6deec6894bd053b282e63cdd42
Walton Lee <mr.waltonlee@me.com>
2017-01-06 01:28:40 +0000

    adding one line of code

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 import hidden_4
+
 name = dir(hidden_4)
 length = len(dir(hidden_4))
-for i in range(0, length):
-    if (name[i][:2] != "__"):
-        print(name[i])
+if __name__ == "__main__":
+    for i in range(0, length):
+        if (name[i][:2] != "__"):
+            print(name[i])
