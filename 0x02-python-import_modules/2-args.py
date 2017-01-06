e4ada7ebe25a85a8c382c95f0d8e5305266341b7
Walton Lee <mr.waltonlee@me.com>
2017-01-06 01:54:20 +0000

    adjusting condition

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,10 +5,10 @@ length = len(sys.argv)
 if __name__ == "__main__":
     if(length - 1 == 0):
         print('{:d} argument.'.format(length - 1))
-    elif(length - 1 <= 1):
+    elif(length - 1 == 1):
         print('{:d} argument: \n{:d}: {:s}'
               .format((length - 1), (length - 1), sys.argv[1]))
-    elif(length > 2):
+    elif(length - 1  >= 2):
         print('{:d} arguments:'.format(length - 1))
         for i in range(1, length):
             print('{:d}: {:s}'.format(i, sys.argv[i]))
