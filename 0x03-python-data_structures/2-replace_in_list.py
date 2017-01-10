5937ef53e469677407eb867c4d46a0ca4c712ce4
Walton Lee <mr.waltonlee@me.com>
2017-01-10 19:55:32 +0000

    pep8 certified

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(list, idx, element):
-    for i in range (len(list)):
+    for i in range(len(list)):
         if (i == idx):
             list[i] = element
             return (list)
