86e080e73514aa1c11b4e024013e9869fd5ac827
Walton Lee <mr.waltonlee@me.com>
2017-01-11 03:23:45 +0000

    considering if the string is none and if it's an empty string

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = 0
+    first_char = None
     if sentence is not None:
-        for i in range(len(sentence)):
+        for i in sentence:
             length += 1
             first_char = sentence[0]
-        return (length, first_char)
-    return (length, None)
+    return (length, first_char)
