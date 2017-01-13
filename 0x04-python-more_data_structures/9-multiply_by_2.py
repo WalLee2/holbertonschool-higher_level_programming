96afd3bef08eeafcb15552349b9a93336dff00f0
Walton Lee <mr.waltonlee@me.com>
2017-01-13 04:27:38 +0000

    finding another way to express the value of the key

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,7 +3,7 @@ def multiply_by_2(my_dict):
     """
     Function that returns a new dictionary with all values multiplied by 2
     """
-    new_dict = my_dict.copy()
-    for k, v in new_dict.items():
-        new_dict.update({k: (v * 2)})
+    new_dict = {}
+    for k in my_dict:
+        new_dict.update({k: (my_dict[k] * 2)})
     return new_dict
