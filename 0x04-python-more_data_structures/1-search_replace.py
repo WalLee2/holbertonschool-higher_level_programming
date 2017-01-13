0b1d65285a8a2dbb216e6ca73dcba75ad31b21c1
Walton Lee <mr.waltonlee@me.com>
2017-01-13 07:01:50 +0000

    Replacing all known instances of a number with another number

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,9 +4,4 @@ def search_replace(my_list, search, replace):
     Function that replaces all occurences of
     an element by another in a new list.
     """
-    new_list = my_list[:]
-    for i in range(len(new_list)):
-        if new_list[i] == search:
-            new_list.pop(new_list[i])
-            new_list.insert(i, replace)
-    return (new_list)
+    return[replace if search == i else i for i in my_list]
