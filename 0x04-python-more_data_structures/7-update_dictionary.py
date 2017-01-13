75cf2488b91e5220d72bc8a220e9e9d6ee4f2ba4
Walton Lee <mr.waltonlee@me.com>
2017-01-13 02:12:40 +0000

    Function that replaces or adds a key value pair in a dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def update_dictionary(my_dict, key, value):
+    """
+    Replacing or adding a key value if the key is there or not
+    """
+    if key in my_dict:
+        my_dict.update({key: value})
+    else:
+        my_dict.update({key: value})
+    return my_dict
