482b7982676d219828f0fbf410ddd75cc0f6b432
Walton Lee <mr.waltonlee@me.com>
2017-01-13 03:14:15 +0000

    it was the space

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(my_dict):
-    for k in sorted(my_dict):
-        print("{:s}:".format(k), my_dict[k])
+    for k, v in sorted(my_dict.items()):
+        print("{}: {}".format(k, v))
