280e3c5797bc872e54085f8d9840fd37b0874512
Walton Lee <mr.waltonlee@me.com>
2017-01-13 01:08:11 +0000

    not allowed to use .items?

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(my_dict):
-    for k, v in sorted(my_dict.items()):
-        print ('{}: {}'.format(k, v))
+    for k in sorted(my_dict):
+        print ('{}: {}'.format(k, my_dict[k]))
