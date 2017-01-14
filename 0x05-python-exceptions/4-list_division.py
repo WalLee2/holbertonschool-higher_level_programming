a585948a22441a2fca00105ed99db3093c78443b
Walton Lee <mr.waltonlee@me.com>
2017-01-14 02:37:10 +0000

    fixing whitespace

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -9,13 +9,13 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             num = my_list_1[i] / my_list_2[i]
         except(ZeroDivisionError):
-            print ("division by 0")
+            print("division by 0")
             tracker = 1
         except(IndexError):
-            print ("out of range")
+            print("out of range")
             tracker = 1
         except(TypeError):
-            print ("wrong type")
+            print("wrong type")
             tracker = 1
         finally:
             if tracker == 1:
