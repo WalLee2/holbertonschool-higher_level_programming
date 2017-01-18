24dcc98022a420f5fe88f2fe3640c43b6a175a1e
Walton Lee <mr.waltonlee@me.com>
2017-01-18 06:36:10 +0000

    Function that adds two integers including test cases in the tests directory

---
+++ 0x06-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Adding two numbers and raising errors if values are not floats or ints.
+"""
+def add_integer(a, b):
+    """
+    Raise TypeError if a or b is a bool.
+    Raise TypeError for every case that is not either an integer or a float.
+    If number is a float convert it to an int.
+    Add two numbers together.
+    """
+    if isinstance(a, bool):
+        raise TypeError("a must be an integer")
+    elif isinstance(b, bool):
+        raise TypeError("b must be an integer")
+    if ((isinstance(a, (int, float))) and isinstance(b, (int, float))):
+        a = int(a)
+        b = int(b)
+        return a + b
+    if (isinstance(a, (int, float)) is False):
+        raise TypeError("a must be an integer")
+    if (isinstance(b, (int, float)) is False):
+        raise TypeError("b must be an integer")
