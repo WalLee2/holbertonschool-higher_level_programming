141d0425d652cf7af0a9baa2f10b6a672ed9954f
Walton Lee <mr.waltonlee@me.com>
2017-01-19 08:57:58 +0000

    adding tests and having a function print the user's name

--- 0x06-python-test_driven_development/3-say_my_name.py
+++ 0x06-python-test_driven_development/3-say_my_name.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
-def say_my_name(first_name, last_name=""):
-    try:
-        if (isinstance(first_name, str) is False):
-            raise TypeError("first_name must be a string")
-        elif (isinstance(last_name, str)) is False:
-            raise TypeError("last_name must be a string")
-        else:
-            print('My name is {:s} {:s}'.format(first_name, last_name))
+"""
+Function that prints "My name is" and the first and last name of a person
+"""
 
+
+def say_my_name(first_name, last_name=""):
+    """
+    Checking if the first and last name are strings if not, it will produce
+    an error.
+    """
+    try:
+        if not isinstance(first_name, str):
+            raise TypeError("first_name must be a string")
+        if not isinstance(last_name, str):
+            raise TypeError("last_name must be a string")
+        print("My name is {} {}".format(first_name, last_name))
     except TypeError as err:
-        print(err)
+        print (err)
