a1737e16c6722093178fd92ceaa677dc83154a24
Walton Lee <mr.waltonlee@me.com>
2017-01-19 23:15:44 +0000

    function that prints a square with # character

---
+++ 0x06-python-test_driven_development/4-print_square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Printing out a square using the "#" symbol.
+"""
+
+
+def print_square(size):
+    """
+    Raising TypeError if the size is not an int or a float or
+    if size is a True or False value.
+    Raising TypeError if the size is less than 0
+    Raising TypeError if size is a float and less than 0
+    """
+    if not isinstance(size, (int, float)) and isinstance(size, bool):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise TypeError("size must be >= 0")
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+    for i in range(0, int(size)):
+        print("#" * int(size))
