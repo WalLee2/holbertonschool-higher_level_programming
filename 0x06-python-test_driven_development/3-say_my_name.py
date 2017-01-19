3d79b7a779eb9d71a91fbd7bedad0982b2ec7929
Walton Lee <mr.waltonlee@me.com>
2017-01-19 09:06:12 +0000

    switching up conditionals

--- 0x06-python-test_driven_development/3-say_my_name.py
+++ 0x06-python-test_driven_development/3-say_my_name.py
@@ -9,11 +9,8 @@ def say_my_name(first_name, last_name=""):
     Checking if the first and last name are strings if not, it will produce
     an error.
     """
-    try:
-        if not isinstance(first_name, str):
-            raise TypeError("first_name must be a string")
-        if not isinstance(last_name, str):
-            raise TypeError("last_name must be a string")
-        print("My name is {} {}".format(first_name, last_name))
-    except TypeError as err:
-        print (err)
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
