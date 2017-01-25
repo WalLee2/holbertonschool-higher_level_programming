1b71c8a881fda639357fd55fc76ef2ccd6ea00f5
Walton Lee <mr.waltonlee@me.com>
2017-01-25 07:12:25 +0000

    changin up conditions

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -15,6 +15,10 @@ class Rectangle:
     """
 
     def __init__(self, width=0, height=0):
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
         if width < 0:
             raise ValueError("width must be >= 0")
         if height < 0:
