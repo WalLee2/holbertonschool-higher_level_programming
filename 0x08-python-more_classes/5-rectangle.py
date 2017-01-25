d746e1d01486184ad0acaf1e22291447ddf41ec7
Walton Lee <mr.waltonlee@me.com>
2017-01-25 06:06:44 +0000

    changing variable

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -30,7 +30,7 @@ class Rectangle:
     def width(self, value):
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        if self.__width < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -40,9 +40,9 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if type(self.__height) is not int:
+        if type(value) is not int:
             raise TypeError("height must be an integer")
-        if self.__height < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
