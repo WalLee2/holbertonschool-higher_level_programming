188d774e888797f9f73d1c8448775a349ed031f7
Walton Lee <mr.waltonlee@me.com>
2017-01-25 07:30:31 +0000

    fixing spacing issue

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -67,4 +67,4 @@ class Rectangle:
         return ("Rectangle({:d}, {:d})".format(self.__width, self.__height))
 
     def __del__(self):
-        print ("Bye rectangle...")
+        print("Bye rectangle...")
