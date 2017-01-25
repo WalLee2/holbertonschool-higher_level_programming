8925eac278cc573dae71fc0fd4ef1001f390d167
Walton Lee <mr.waltonlee@me.com>
2017-01-25 07:23:51 +0000

    work please

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -25,6 +25,10 @@ class Rectangle:
     def width(self):
         return self.__width
 
+    @property
+    def height(self):
+        return self.__height
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -32,10 +36,6 @@ class Rectangle:
         if self.__width < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-
-    @property
-    def height(self):
-        return self.__height
 
     @height.setter
     def height(self, value):
