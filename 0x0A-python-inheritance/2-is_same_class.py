27cf0b75b062d878535eeecbe9f2f0036c68cbd5
Walton Lee <mr.waltonlee@me.com>
2017-01-31 07:45:29 +0000

    pep8 approved

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Function returns True if the object is exactly an instance of the specified class.
+Function returns True if the object is
+exactly an instance of the specified class.
 """
 
 
