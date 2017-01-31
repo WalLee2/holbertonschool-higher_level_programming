ac65e309216af35879f56dffcf3b438de61fb054
Walton Lee <mr.waltonlee@me.com>
2017-01-31 07:48:14 +0000

    pep8 approved

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -9,7 +9,7 @@ class Rectangle(BaseGeometry):
     """
     Inheriting from BaseGeometry
     """
-    
+
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.integer_validator("height", height)
