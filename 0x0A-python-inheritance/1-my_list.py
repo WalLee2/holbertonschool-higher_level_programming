1429bbafcb3f90689aa50821c771cbce9c1dc85f
Walton Lee <mr.waltonlee@me.com>
2017-01-31 07:17:37 +0000

    Writing a class MyList that inherits from list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Inherting from a list
+"""
+
+class MyList(list):
+
+    def print_sorted(self):
+        """
+        sorting a list.
+        """
+        print (sorted(self))
