7eadf05df1c51030672581e262f7c0311020a564
Walton Lee <mr.waltonlee@me.com>
2017-02-04 01:03:49 +0000

    Return the number lines of a text file.

---
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Return the number of lines of a text file.
+"""
+
+
+def number_of_lines(filename=""):
+    """
+    Open file and add 1 to counter for each line read.
+    """
+    with open("my_file_0.txt", 'r', encoding="UTF-8") as my_file:
+        lineCount = 0
+        while True:
+            line = my_file.readline()
+            if not line:
+                break
+            lineCount += 1
+        return (lineCount)
