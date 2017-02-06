f5aae5fc98bf8488edd474d046b5e1becfd2d1b2
Walton Lee <mr.waltonlee@me.com>
2017-02-06 20:05:27 +0000

    covering an edge case that was not necessary.

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -10,10 +10,4 @@ def write_file(filename="", text=""):
     Opening the file in read mode and returning the number of characters.
     """
     with open(filename, mode="w", encoding="UTF-8") as my_file:
-        my_file.write(text)
-    with open(filename, 'r', encoding="UTF-8") as my_file:
-        c_count = 0
-        for i in my_file:
-            for j in i:
-                c_count += 1
-        return (c_count)
+        return (my_file.write(text))
