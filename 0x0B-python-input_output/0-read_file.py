c15b16a39391ab995da04f0b36adc056a2f58d79
Walton Lee <mr.waltonlee@me.com>
2017-02-06 19:49:24 +0000

    Reading a text file and prints to stdout.

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Reading a text file in UTF-8 format and prints to standard out.
+"""
+
+
+def read_file(filename=""):
+    """
+    opening text file and printing to standard out.
+    """
+    with open("my_file_0.txt", 'r', encoding="UTF-8") as my_file_0:
+        print(my_file_0.read(), end="")
