0b1a54b8e9293dfca094065866f026cc9e1aa760
Walton Lee <mr.waltonlee@me.com>
2017-02-23 00:35:32 +0000

    Listing all records with a score equal or greater to 10

---
+++ 0x0C-SQL_introduction/11-best_score.sql
@@ -0,0 +1,4 @@
+-- Listing all records with a score above 10 in the second table
+-- Ordering the second table by the highest score
+-- that's greater than or equal to 10
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
