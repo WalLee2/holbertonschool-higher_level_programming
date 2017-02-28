67a1503073805268b31186d9f10b5130e2561383
Walton Lee <mr.waltonlee@me.com>
2017-02-28 23:55:12 +0000

    order by ascending

--- 0x0D-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0D-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- List all the cities of California found in the database htbn_0d_usa
 
-SELECT id, name FROM hbtn_0d_usa.cities WHERE state_id = (SELECT id FROM hbtn_0d_usa.states WHERE name = 'California');
+SELECT id, name FROM hbtn_0d_usa.cities WHERE state_id = (SELECT id FROM hbtn_0d_usa.states WHERE name = 'California') ORDER BY id;
