15cd90c967891e364e96e0a5487e247dd142b819
Walton Lee <mr.waltonlee@me.com>
2017-03-01 04:33:58 +0000

    fixing ordering by

--- 0x0D-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0D-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,2 @@
 -- Importing database dump and listing all shows contained in the dump file
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows JOIN tv_shows ON tv_shows.title = tv_show_genres.genre_id ORDER BY tv_shows.title ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows JOIN tv_shows ON tv_shows.title = tv_show_genres.genre_id ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
