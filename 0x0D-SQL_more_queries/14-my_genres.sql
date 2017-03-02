342a2aa73e30eb0ca98c63ee721fe1d9d06c661b
Walton Lee <mr.waltonlee@me.com>
2017-03-02 00:53:30 +0000

    List table that contains only one record where the title is equal to Dexter

---
+++ 0x0D-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,7 @@
+-- Importing database dump and listing all shows contained in the dump file
+SELECT tv_genres.name
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_shows.title = "Dexter"
+ORDER BY tv_genres.name ASC;
