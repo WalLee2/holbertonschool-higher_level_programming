d1ee0cd600385a0bdb117eae6760056e037c569d
Walton Lee <mr.waltonlee@me.com>
2017-03-09 01:02:03 +0000

    fixing formatting

--- 0x0E-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0E-python-object_relational_mapping/4-cities_by_state.py
@@ -2,11 +2,14 @@
 import sys
 import MySQLdb
 
-db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-cursor = db.cursor()
-cursor.execute(
-"SELECT cities.id, cities.name, states.name FROM states JOIN cities ON cities.state_id = states.id ORDER BY cities.id"
-)
-for row in cursor.fetchall():
-    print(row)
-db.close()
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute(
+        "SELECT cities.id, cities.name, states.name FROM states\
+        JOIN cities ON cities.state_id = states.id ORDER BY cities.id"
+    )
+    for row in cursor.fetchall():
+        print(row)
+    db.close()
+    cursor.close()
