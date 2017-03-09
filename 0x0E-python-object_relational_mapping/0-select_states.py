b69a51129479c3a67c4ec13d0a7f4ab1cba407ad
Walton Lee <mr.waltonlee@me.com>
2017-03-09 00:27:00 +0000

    removing the space in between my print statement

--- 0x0E-python-object_relational_mapping/0-select_states.py
+++ 0x0E-python-object_relational_mapping/0-select_states.py
@@ -7,6 +7,6 @@ if __name__ == '__main__':
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
     for row in cursor.fetchall():
-        print (row)
+        print(row)
     db.close()
     cursor.close()
