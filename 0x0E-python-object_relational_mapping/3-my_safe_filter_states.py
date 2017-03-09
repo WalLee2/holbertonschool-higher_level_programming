366a60b10dd9e99ae4908ad072f9a14177239d20
Walton Lee <mr.waltonlee@me.com>
2017-03-09 00:58:40 +0000

    safe

---
+++ 0x0E-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states")
+    find = sys.argv[4]
+    for row in cursor.fetchall():
+        if (row[1] == find):
+            print (row)
+    db.close()
+    cursor.close()
