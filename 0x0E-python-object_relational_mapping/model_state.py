9808a6b9b6d17a43fd50ce42985451f20935a5f9
Walton Lee <mr.waltonlee@me.com>
2017-03-09 01:47:07 +0000

    removing extra code

--- 0x0E-python-object_relational_mapping/model_state.py
+++ 0x0E-python-object_relational_mapping/model_state.py
@@ -6,6 +6,8 @@ Base = declarative_base()
 
 
 class State(Base):
-    __tablename__ = 'states'
-    id = Column("states", Integer, primary_key=True, autoincrement=True)
-    name = Column("states", String(128), nullable=False)
+    if __name__ == '__main__':
+        __tablename__ = 'states'
+        id = Column(Integer, primary_key=True,
+                    autoincrement=True, nullable=False)
+        name = Column(String(128), nullable=False)
