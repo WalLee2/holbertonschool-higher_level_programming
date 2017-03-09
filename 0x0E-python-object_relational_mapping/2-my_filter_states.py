6a30f6c1df0f18dcd56ad7017e6cfe556f9346ae
Walton Lee <mr.waltonlee@me.com>
2017-03-09 00:55:17 +0000

    double checking in the for loop

--- 0x0E-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0E-python-object_relational_mapping/2-my_filter_states.py
@@ -2,11 +2,14 @@
 import sys
 import MySQLdb
 
-db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-cursor = db.cursor()
-cursor.execute("SELECT * FROM states")
-find = sys.argv[4]
-for row in cursor.fetchall():
-    if (row[1] == find):
-        print (row)
-db.close()
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    check = "SELECT * FROM states WHERE name = '{:s}'\
+    ORDER BY id ASC".format(sys.argv[4])
+    cursor.execute(check)
+    for row in cursor.fetchall():
+        if (row[1] == sys.argv[4]):
+            print (row)
+    db.close()
+    cursor.close()
