7b461f642561821e14f4e67ea33643e78a9bff69
Walton Lee <mr.waltonlee@me.com>
2017-03-09 05:25:05 +0000

    If the argument passed exists, print the id number

--- 0x0E-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0E-python-object_relational_mapping/10-model_state_my_get.py
@@ -11,6 +11,7 @@ if __name__ == '__main__':
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
+    switch = 0
     for objects in session.query(State):
         if (objects.name == sys.argv[4]):
             print("{}".format(objects.id))
