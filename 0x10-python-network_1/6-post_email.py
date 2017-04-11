628acfedd00d2b88389634d6df3ea49258886295
Walton Lee <mr.waltonlee@me.com>
2017-04-11 22:57:59 +0000

    adding an extra space

--- 0x10-python-network_1/6-post_email.py
+++ 0x10-python-network_1/6-post_email.py
@@ -2,9 +2,9 @@
 """
 Sends a POST request to the passed URL passing email as a parameter
 """
-
 import requests
 import sys
+
 
 r = requests.post(sys.argv[1], data={'email': sys.argv[2]})
 print(r.text)
