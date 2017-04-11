8c50dfbf31669fbfcf066c0a67bb9f9b6b5bc755
Walton Lee <mr.waltonlee@me.com>
2017-04-11 20:23:36 +0000

    importting wrong?

--- 0x10-python-network_1/0-hbtn_status.py
+++ 0x10-python-network_1/0-hbtn_status.py
@@ -2,10 +2,10 @@
 """
 Script that fetches a URL
 """
-from urllib.request import urlopen
+import urllib.request
 
-if __name__ == '__main__':
-    with urlopen('https://intranet.hbtn.io/status') as my_req:
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as my_req:
         open_page1 = my_req.read()
         print("Body response:")
         print("\t- type: {}".format(type(open_page1)))
