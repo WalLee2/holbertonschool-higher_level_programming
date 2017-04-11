3a44ec30de88f08e22509eb19ef7ec93a6f2c421
Walton Lee <mr.waltonlee@me.com>
2017-04-11 23:11:19 +0000

    adding an if condition

--- 0x10-python-network_1/7-error_code.py
+++ 0x10-python-network_1/7-error_code.py
@@ -6,8 +6,9 @@ import sys
 import requests
 
 
-try:
-    r = requests.get(sys.argv[1])
-    print(r.text)
-except requests.exceptions.HTTPError:
-    print("Error code: {}".format(r.status_code))
+if __name__ == "__main__":
+    try:
+        r = requests.get(sys.argv[1])
+        print(r.text)
+    except requests.exceptions.HTTPError:
+        print("Error code: {}".format(r.status_code))
