c670239c960a85764876ff5ec59d86dd129d8c7a
Walton Lee <mr.waltonlee@me.com>
2017-04-11 23:22:14 +0000

    takes in a URL and displays the body of the response

--- 0x10-python-network_1/7-error_code.py
+++ 0x10-python-network_1/7-error_code.py
@@ -7,8 +7,8 @@ import requests
 
 
 if __name__ == "__main__":
-    try:
-        r = requests.get(sys.argv[1])
+    r = requests.get(sys.argv[1])
+    if r.status_code == requests.codes.ok
         print(r.text)
-    except requests.exceptions.HTTPError:
+    else:
         print("Error code: {}".format(r.status_code))
