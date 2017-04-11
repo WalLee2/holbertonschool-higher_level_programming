d26e49721c5133ae5b1a76fc23c16e8220b28223
Walton Lee <mr.waltonlee@me.com>
2017-04-11 21:28:56 +0000

    modifying code

--- 0x10-python-network_1/2-post_email.py
+++ 0x10-python-network_1/2-post_email.py
@@ -8,8 +8,8 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    values = {'email': sys.argv[2]}
-    data = urllib.parse.urlencode(values)
+    email = sys.argv[2]
+    data = urllib.parse.urlencode('email': email)
     data = data.encode('ascii')
     with urllib.request.urlopen(sys.argv[1], data) as my_req:
         post_req = (my_req.read()).decode('utf-8')
