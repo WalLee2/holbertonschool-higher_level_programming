bcd52a5b444e706069462e13ed7913e1b29b1bd1
Walton Lee <mr.waltonlee@me.com>
2017-04-11 22:59:26 +0000

    preventing file from running upon import

--- 0x10-python-network_1/6-post_email.py
+++ 0x10-python-network_1/6-post_email.py
@@ -5,6 +5,6 @@ Sends a POST request to the passed URL passing email as a parameter
 import requests
 import sys
 
-
-r = requests.post(sys.argv[1], data={'email': sys.argv[2]})
-print(r.text)
+if __name__ == "__main__":
+    r = requests.post(sys.argv[1], data={'email': sys.argv[2]})
+    print(r.text)
