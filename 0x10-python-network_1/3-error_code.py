5da77d9a0f4da58a15ea494a930c3ca0949b3da3
Walton Lee <mr.waltonlee@me.com>
2017-04-11 22:05:24 +0000

    fixing parenthesis and pep8

--- 0x10-python-network_1/3-error_code.py
+++ 0x10-python-network_1/3-error_code.py
@@ -6,10 +6,11 @@ import urllib.request
 import urllib.error
 import sys
 
+
 if __name__ == "__main__":
     try:
         with urllib.request.urlopen(sys.argv[1]) as my_req:
-            my_req = (my_req.read()).decode('utf-8'))
+            my_req = (my_req.read()).decode('utf-8')
             print(my_req)
     except urllib.error.HTTPError as error:
         print("Error code: {}".format(error.code))
