74c388056f994e553d9557888b0d8feecc3b2107
Walton Lee <mr.waltonlee@me.com>
2017-04-11 21:56:22 +0000

    changing the print statement

--- 0x10-python-network_1/3-error_code.py
+++ 0x10-python-network_1/3-error_code.py
@@ -7,7 +7,8 @@ import sys
 
 try:
     with urllib.request.urlopen(sys.argv[1]) as my_req:
-        print(my_req.read().decode('utf-8'))
+        my_req = (my_req.read()).decode('utf-8'))
+        print(my_req)
 
 except urllib.error.HTTPError as error:
     print("Error code: {}".format(error.code))
