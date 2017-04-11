5415aea1da13a1ed186568aae32872547df4573a
Walton Lee <mr.waltonlee@me.com>
2017-04-11 05:46:57 +0000

    silencing

--- 0x0F-python-network_0/41-post_params.sh
+++ 0x0F-python-network_0/41-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes a URL, sends a POST request to the URL, and display response
-curl --data "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
+curl -sd "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
