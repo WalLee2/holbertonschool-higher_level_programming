02d39cce946922b65851f0fc38170e058d188df6
Walton Lee <mr.waltonlee@me.com>
2017-04-11 21:34:29 +0000

    storing the url in a variable

--- 0x10-python-network_1/2-post_email.py
+++ 0x10-python-network_1/2-post_email.py
@@ -8,9 +8,10 @@ import urllib.request
 
 
 if __name__ == "__main__":
+    url = sys.argv[1]
     email = sys.argv[2]
     data = urllib.parse.urlencode({'email': email})
     data = data.encode('ascii')
-    with urllib.request.urlopen(sys.argv[1], data) as my_req:
+    with urllib.request.urlopen(url, data) as my_req:
         post_req = (my_req.read()).decode('utf-8')
         print(post_req)
