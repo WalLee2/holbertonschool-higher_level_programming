7f34125d1703109c46064d07455ad242f1aa6643
Walton Lee <mr.waltonlee@me.com>
2017-04-11 22:28:16 +0000

    adding condition to not run my code after importing

--- 0x10-python-network_1/5-hbtn_header.py
+++ 0x10-python-network_1/5-hbtn_header.py
@@ -5,8 +5,8 @@ Takes in a URL and displays the value of the variable X-Request-Id
 import requests
 import sys
 
-r = requests.get(sys.argv[1])
-
-for key, values in r.headers.items():
-    if key == "X-Request-Id":
-        print(values)
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
+    for key, values in r.headers.items():
+        if key == "X-Request-Id":
+            print(values)
