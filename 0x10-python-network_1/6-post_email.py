452c0500e5b787ea3da4cd352296e976eddc6e56
Walton Lee <mr.waltonlee@me.com>
2017-04-11 22:51:14 +0000

    Sending a post request to a URL and display the body of the response

---
+++ 0x10-python-network_1/6-post_email.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Sends a POST request to the passed URL passing email as a parameter
+"""
+
+import requests
+import sys
+
+r = requests.post(sys.argv[1], data = {'email':sys.argv[2]})
+print(r.content.decode("utf-8"))
