6077da2e172284aaa829c8993563d5b840715962
Walton Lee <mr.waltonlee@me.com>
2017-04-11 05:45:43 +0000

    adjusting output

--- 0x0F-python-network_0/41-post_params.sh
+++ 0x0F-python-network_0/41-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Script that takes a URL, sends a POST request to the URL, and display response
-curl --data "email=hr@holbertonschool.com&subject="\
-"I will always be here for PLD" "$1"
+curl --data "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
