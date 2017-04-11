2aad0f92c954e4dd364d31e5af9114be46ca3606
Walton Lee <mr.waltonlee@me.com>
2017-04-11 22:52:52 +0000

    fixing pep8 and making file executable

--- 0x10-python-network_1/6-post_email.py
+++ 0x10-python-network_1/6-post_email.py
@@ -6,5 +6,5 @@ Sends a POST request to the passed URL passing email as a parameter
 import requests
 import sys
 
-r = requests.post(sys.argv[1], data = {'email':sys.argv[2]})
+r = requests.post(sys.argv[1], data={'email': sys.argv[2]})
 print(r.content.decode("utf-8"))
