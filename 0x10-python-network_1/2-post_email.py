062af67a1891cd611fd35c714c7cf0b94a196347
Walton Lee <mr.waltonlee@me.com>
2017-04-11 21:14:58 +0000

    sending a post request to a URL

---
+++ 0x10-python-network_1/2-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Sends a POST request to the passed URL
+"""
+import sys
+import urllib.parse
+import urllib.request
+
+
+if __name__ == "__main__":
+    values = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    with urllib.request.urlopen(sys.argv[1], data) as my_req:
+        post_req = my_req.read().decode('utf-8')
+    print(post_req)
