eec457b9409101972ae39966b844fbeeeee6cc10
Walton Lee <mr.waltonlee@me.com>
2017-04-11 23:10:29 +0000

    Display the body of the response of sending a request to the URL

---
+++ 0x10-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Takes in a URL and displays the body of the response
+"""
+import sys
+import requests
+
+
+try:
+    r = requests.get(sys.argv[1])
+    print(r.text)
+except requests.exceptions.HTTPError:
+    print("Error code: {}".format(r.status_code))
