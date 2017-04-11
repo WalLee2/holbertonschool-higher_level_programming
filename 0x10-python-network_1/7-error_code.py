18c792fe4dd673b8272a0c78dc536394eae3f6ab
Walton Lee <mr.waltonlee@me.com>
2017-04-11 23:23:55 +0000

    adding a colon to the if statement

--- 0x10-python-network_1/7-error_code.py
+++ 0x10-python-network_1/7-error_code.py
@@ -8,7 +8,7 @@ import requests
 
 if __name__ == "__main__":
     r = requests.get(sys.argv[1])
-    if r.status_code == requests.codes.ok
+    if r.status_code == requests.codes.ok:
         print(r.text)
     else:
         print("Error code: {}".format(r.status_code))
