c2852c1ddda0e7cde43ab2c93d1451567f3ac058
Walton Lee <mr.waltonlee@me.com>
2017-04-12 02:08:59 +0000

    adjusting the print statement in try block

--- 0x10-python-network_1/8-json_api.py
+++ 0x10-python-network_1/8-json_api.py
@@ -8,11 +8,13 @@ import sys
 
 
 if __name__ == "__main__":
-        if len(sys.argv[1]) != 2 or not sys.argv[1].isalpha():
+        if len(sys.argv) != 2 or not sys.argv[1].isalpha():
                 print("No result")
-        url = 'http://0.0.0.0:5000/search_user'
-        r = requests.post(url, data={'q': sys.argv[1]})
-        try:
-            print("[{}] {}".format(r.json().get('id'), r.jsonj()('name')))
-        except Exception:
-            print("Not a valid JSON")
+        else:
+                url = 'http://0.0.0.0:5000/search_user'
+                r = requests.post(url, data={'q': sys.argv[1]})
+                js = r.json()
+                try:
+                        print("[{}] {}".format(js.get('id'), js.get('name')))
+                except Exception:
+                        print("Not a valid JSON")
