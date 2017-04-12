0b2e3be8382198ec9da495f300778c8c0410ab99
Walton Lee <mr.waltonlee@me.com>
2017-04-12 02:05:18 +0000

    adjusting data variable

--- 0x10-python-network_1/8-json_api.py
+++ 0x10-python-network_1/8-json_api.py
@@ -11,8 +11,7 @@ if __name__ == "__main__":
         if len(sys.argv[1]) != 2 or not sys.argv[1].isalpha():
                 print("No result")
         url = 'http://0.0.0.0:5000/search_user'
-        q = {"q": sys.argv[1]}
-        r = requests.post(url, data=q)
+        r = requests.post(url, data={'q': sys.argv[1]})
         try:
             print("[{}] {}".format(r.json().get('id'), r.jsonj()('name')))
         except Exception:
