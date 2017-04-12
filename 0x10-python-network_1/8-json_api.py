e969927a087aab894889b8ce57d8c22d8dbca035
Walton Lee <mr.waltonlee@me.com>
2017-04-12 02:01:57 +0000

    getting correct output

--- 0x10-python-network_1/8-json_api.py
+++ 0x10-python-network_1/8-json_api.py
@@ -8,13 +8,12 @@ import sys
 
 
 if __name__ == "__main__":
-        if not sys.argv[1] or len(sys.argv[1]) < 2\
-           or not sys.argv[1].isalpha():
-            print("No result")
+        if len(sys.argv[1]) != 2 or not sys.argv[1].isalpha():
+                print("No result")
         url = 'http://0.0.0.0:5000/search_user'
         q = {"q": sys.argv[1]}
         r = requests.post(url, data=q)
         try:
-            print("[{}] {}".format(r.json()['id'], r.jsonj()['name']))
+            print("[{}] {}".format(r.json().get('id'), r.jsonj()('name')))
         except Exception:
             print("Not a valid JSON")
