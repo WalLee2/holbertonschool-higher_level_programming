cc5437f32832caa59589a608549192daf2f61d3b
Walton Lee <mr.waltonlee@me.com>
2017-04-27 20:34:39 +0000

    Print 3 lines by using an array of strings and a loop

---
+++ 0x11-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+let myArray = new Array(3);
+
+myArray[0] = 'C is fun';
+myArray[1] = 'Python is cool';
+myArray[2] = 'Javascript is amazing';
+for (let i = 0; i < 3; i++) {
+  console.log(myArray[i]);
+}
