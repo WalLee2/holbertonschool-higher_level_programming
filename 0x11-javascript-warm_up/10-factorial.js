bc01cf52169ebe296de04cdbe4a82342a756d0e6
Walton Lee <mr.waltonlee@me.com>
2017-04-27 22:36:32 +0000

    semistandard approved

--- 0x11-javascript-warm_up/10-factorial.js
+++ 0x11-javascript-warm_up/10-factorial.js
@@ -2,12 +2,12 @@
 
 if (Number(process.argv[2])) {
   let usrIn = Number(process.argv[2]);
-  let count = 1
+  let count = 1;
   for (let i = usrIn; i > 0; i--) {
     count = usrIn * count;
     usrIn--;
   }
   console.log(count);
 } else {
-  console.log("1");
+  console.log('1');
 }
