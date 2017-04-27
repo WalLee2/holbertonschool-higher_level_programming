e4fcee9ecdb0ddb112f4b0acbfec745fa8c11be5
Walton Lee <mr.waltonlee@me.com>
2017-04-27 22:35:05 +0000

    fixing spacing error

--- 0x11-javascript-warm_up/8-square.js
+++ 0x11-javascript-warm_up/8-square.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 
 if (Number(process.argv[2])) {
-    let usrIn = Number(process.argv[2]);
-    let myStr = 'X';
-    for (let i = 1; i < usrIn; i++) {
-      myStr = myStr + 'X';
-    }
-    for (let a = 0; a < usrIn; a++) {
-      console.log(myStr);
-    }
+  let usrIn = Number(process.argv[2]);
+  let myStr = 'X';
+  for (let i = 1; i < usrIn; i++) {
+    myStr = myStr + 'X';
+  }
+  for (let a = 0; a < usrIn; a++) {
+    console.log(myStr);
+  }
 } else {
-    console.log('Missing size');
+  console.log('Missing size');
 }
