edc9e516e71f191ca89c2a7345bfb964d7f9cf62
Walton Lee <mr.waltonlee@me.com>
2017-04-28 02:09:37 +0000

    changing to recursion

--- 0x11-javascript-warm_up/10-factorial.js
+++ 0x11-javascript-warm_up/10-factorial.js
@@ -1,17 +1,9 @@
 #!/usr/bin/node
 
 function factorial (a) {
-  if (a) {
-    let usrIn = Number(process.argv[2]);
-    let count = 1;
-    for (let i = usrIn; i > 0; i--) {
-      count = usrIn * count;
-      usrIn--;
-    }
-    return (count);
-  } else {
-    return (1);
+  if (a === 0 || isNaN(a) || a <= 1) {
+   return (1);
   }
+  return (a * factorial(a - 1));
 }
-
-console.log(factorial(process.argv[2]));
+console.log(factorial(Number(process.argv[2])));
