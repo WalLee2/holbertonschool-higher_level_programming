299097ceaace0ed93f691361be72c7002383bd8c
Walton Lee <mr.waltonlee@me.com>
2017-04-30 22:26:32 +0000

    moving print statement and setting conditions when to print

--- 0x12-javascript-web_scraping/4-rectangle.js
+++ 0x12-javascript-web_scraping/4-rectangle.js
@@ -5,6 +5,13 @@ exports.Rectangle = function (w, h) {
     this.width = w;
     this.height = h;
   }
+  this.print = function () {
+    if (w > 0 && h > 0) {
+      for (let i = 0; i < h; i++) {
+        console.log('X'.repeat(w));
+      }
+    }
+  };
   this.rotate = function () {
     let temp = this.width;
     this.width = this.height;
@@ -13,10 +20,5 @@ exports.Rectangle = function (w, h) {
   this.double = function () {
     this.width = this.width * 2;
     this.height = this.height * 2;
-  };
-  this.print = function () {
-    for (let i = 0; i < h; i++) {
-      console.log('X'.repeat(w));
-    }
   };
 };
