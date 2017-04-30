ee8e95867965d32e2a8bff7a577bd5c3e36ca37a
Walton Lee <mr.waltonlee@me.com>
2017-04-30 21:20:42 +0000

    Display status code of a GET request

---
+++ 0x12-javascript-web_scraping/9-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+var request = require('request');
+request(process.argv[2], function (error, response) {
+  if (error) {
+    console.log('code: ', error.statusCode);
+  } else {
+    console.log('code: ', response.statusCode);
+  }
+});
