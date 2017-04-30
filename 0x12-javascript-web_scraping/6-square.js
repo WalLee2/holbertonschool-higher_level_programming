97db21aa6ae707119d1174cb5ddee9db233fe9fe
Walton Lee <mr.waltonlee@me.com>
2017-04-30 22:38:32 +0000

    fixing errors

--- 0x12-javascript-web_scraping/6-square.js
+++ 0x12-javascript-web_scraping/6-square.js
@@ -4,10 +4,12 @@ const s = require('./5-square').Square;
 exports.Square = function (size) {
   s.call(this, size);
 };
+
 exports.Square.prototype = Object.create(exports.Square.prototype);
-exports.Square.prototype.constructor = exports.Square;
+exports.Square.constructor = exports.Square;
+
 exports.Square.prototype.charPrint = function (c = 'X') {
-  for (let i = 0; i < this.size; i++) {
-    console.log(c.repeat(this.size));
+  for (let i = 0; i < this.height; i++) {
+    console.log(c.repeat(this.width));
   }
 };
