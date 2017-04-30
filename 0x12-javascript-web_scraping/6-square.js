c4cd5881926f148703190389c4a213bc7e7287ad
Walton Lee <mr.waltonlee@me.com>
2017-04-30 21:58:38 +0000

    reformatting instance method

--- 0x12-javascript-web_scraping/6-square.js
+++ 0x12-javascript-web_scraping/6-square.js
@@ -3,17 +3,18 @@
 const s = require('./5-square').Square;
 exports.Square = function (size) {
   s.call(this, size);
-  this.charPrint = function (c) {
-    if (c === undefined || c === null) {
-      for (let i = 0; i < this.size; i++) {
-        console.log('X'.repeat(this.size));
-      }
-    } else {
-      for (let i = 0; i < this.size; i++) {
-        console.log(c.repeat(this.size));
-      }
-    }
-  };
 };
 exports.Square.prototype = Object.create(exports.Square.prototype);
 exports.Square.prototype.constructor = exports.Square;
+
+exports.Square.prototype.charPrint = function (c) {
+  if (c === undefined || c === null) {
+    for (let i = 0; i < this.size; i++) {
+      console.log('X'.repeat(this.size));
+    }
+  } else {
+    for (let i = 0; i < this.size; i++) {
+      console.log(c.repeat(this.size));
+    }
+  }
+}
