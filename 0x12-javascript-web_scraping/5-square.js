f3aaa3734f4d590884945d31ce14a6f34b5b89ec
Walton Lee <mr.waltonlee@me.com>
2017-04-30 21:16:25 +0000

    Square class that inherits from rectangle

---
+++ 0x12-javascript-web_scraping/5-square.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle').Rectangle;
+exports.Square = function (size) {
+  Rectangle.call(this, size, size);
+  this.size = size;
+};
+exports.Square.prototype = Object.create(Rectangle.prototype);
+exports.Square.prototype.constructor = exports.Square;
