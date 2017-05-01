faaa5fe8a258828780735ffd50f71a6efd7d860e
Walton Lee <mr.waltonlee@me.com>
2017-05-01 05:31:40 +0000

    fixing spacing

--- 0x12-javascript-web_scraping/9-statuscode.js
+++ 0x12-javascript-web_scraping/9-statuscode.js
@@ -3,8 +3,8 @@
 var request = require('request');
 request(process.argv[2], function (error, response) {
   if (error) {
-    console.log('code: ', error.statusCode);
+    console.log('code:', error.statusCode);
   } else {
-    console.log('code: ', response.statusCode);
+    console.log('code:', response.statusCode);
   }
 });
