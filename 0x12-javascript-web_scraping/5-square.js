d81dcd0269827962fa7c591060d658d939bc0a19
Walton Lee <mr.waltonlee@me.com>
2017-05-01 00:29:53 +0000

    commenting out extraneous code

--- 0x12-javascript-web_scraping/5-square.js
+++ 0x12-javascript-web_scraping/5-square.js
@@ -4,8 +4,8 @@ const Rectangle = require('./4-rectangle').Rectangle;
 function Square (size) {
   Rectangle.call(this, size, size);
 }
-Square.prototype = Object.create(Rectangle.prototype);
-Square.constructor = Square;
+/*Square.prototype = Object.create(Rectangle.prototype);
+Square.constructor = Square;*/
 
 exports.Rectangle = Rectangle;
 exports.Square = Square;
